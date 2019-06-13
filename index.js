dcbec86773505a418a85019493fe1db66c5d13b7
taggar <rvdeede@gmail.com>
2019-06-13 14:37:26 +0200

    Switched to mongoose and ejs; now successfully connecting to mongodb atlas

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express'),
     PORT = 5000,
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    mongoose = require("mongoose"),
+    Quote = require("./models/quote");
 
 // initialize express
 const app = express();
@@ -8,8 +10,18 @@ const app = express();
 // enable parsing of posted data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
 
+// db connection
+const options = {
+    keepAlive: 1,
+    connectTimeoutMS: 30000,
+    useNewUrlParser: true
+};
 
+const uri = "mongodb+srv://dbuser:dbuser@becode-lhupj.gcp.mongodb.net/quotes?retryWrites=true&w=majority";
+
+mongoose.connect(uri, options);
 
 // import the route handling module
 require("./routes/quoteRoutes")(app);
@@ -18,3 +30,4 @@ require("./routes/quoteRoutes")(app);
 app.listen(PORT, () => {
     console.log(`Server running`);
 });
+
